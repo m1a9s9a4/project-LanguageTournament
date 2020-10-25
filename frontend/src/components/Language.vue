0df7ce1ad9f2e15d85356f5115f20aa23309c4fb
masafumi kato <xslash1105@gmail.com>
2020-10-25 23:12:48 +0900

    [update] added vuetify

---
+++ frontend/src/components/Language.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-img
+    lazy-src="https://picsum.photos/id/11/10/6"
+    max-height="150"
+    max-width="250"
+    src="https://picsum.photos/id/11/500/300"
+    ></v-img>
+</template>
