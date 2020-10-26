836612e5ee00d98a12cb7178ce4af3e1d4e53ac7
masafumi kato <xslash1105@gmail.com>
2020-10-26 20:50:00 +0900

    [add] Home View

---
+++ frontend/src/Home.vue
@@ -0,0 +1,23 @@
+/* eslint-disable */
+<template>
+  <v-main>
+    <h2>home</h2>
+    <v-row>
+    <v-col md="3" cols="6" v-for="n in 10" v-bind:key="n">
+        <Language 
+            name="php"
+        />
+    </v-col>
+    </v-row>
+  </v-main>
+</template>
+
+<script>
+import Language from './components/Home/Language';
+
+export default {
+    components: {
+        Language,
+    },
+}
+</script>
