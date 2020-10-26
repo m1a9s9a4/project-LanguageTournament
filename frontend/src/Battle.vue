fd3e56fff76b2412bff6e2b0eba279294e55355a
masafumi kato <xslash1105@gmail.com>
2020-10-26 20:51:19 +0900

    [add] Comparing language page created

---
+++ frontend/src/Battle.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-main>
+      <Question :question="question" />
+      <v-row class="align-center">
+        <v-col md="5" cols="12">
+          <Language 
+            :name="language_1"
+            imgsrc="https://www.php.net//images/logos/new-php-logo.svg"
+          />
+        </v-col>
+        <v-col class="display-3" cols="12">
+          <div class="text-center">
+            or
+          </div>
+        </v-col>
+        <v-col md="5" cols="12">
+          <Language
+            :name="language_2"
+            imgsrc="https://golang.org/lib/godoc/images/go-logo-blue.svg"
+          />
+        </v-col>
+      </v-row>
+    </v-main>
+</template>
+
+<script>
+import Question from './components/Battle/Question';
+import Language from './components/Battle/Language';
+
+export default {
+  name: 'App',
+
+  components: {
+    Question,
+    Language,
+  },
+
+  data: () => ({
+    question: '',
+  }),
+
+  computed: {
+    language_1: function () {
+      return this.$route.params["l1"];
+    },
+    language_2: function () {
+      return this.$route.params["l2"];
+    }
+  },
+
+  created: function () {
+    this.getQuestion();
+  },
+
+  methods: {
+    getQuestion: function () {
+      this.question = '質問が入ります';
+    },
+
+  }
+};
+</script>
---
+++ frontend/src/components/Battle/Question.vue
@@ -0,0 +1,14 @@
+<template>
+    <p class="subtitle-1 text-center mt-5 pa-5" v-text="question"></p>
+</template>
+
+<script>
+export default {
+    props: {
+        'question': {
+            type: String,
+            default: 'hogehgoe',
+        }
+    },
+}
+</script>
