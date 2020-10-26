b06a7c30f56067054c5646f090b1e1029ba5fc4c
masafumi kato <xslash1105@gmail.com>
2020-10-26 20:51:49 +0900

    [add] Detail page created

---
+++ frontend/src/Detail.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-main>
+        <h2>{{ language }} is Compared with...</h2>
+        <v-row>
+            <v-col cols="12" md="3">
+                <Language />
+            </v-col>
+        </v-row>
+    </v-main>
+</template>
+
+<script>
+import Language from './components/Detail/Language';
+
+export default {
+    computed: {
+        language: function () {
+        return this.$route.params["lang"];
+        },
+    },
+    components: {
+        Language,
+    },
+}
+</script>
---
+++ frontend/src/components/Detail/Language.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card class="mx-auto" max-width="300" elevation="10">
+        <a href="#">
+          <v-img contain :alt="name" height="200" :src="imgsrc"></v-img>
+        </a>
+    </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "vuetify",
+    },
+    imgsrc: {
+      type: String,
+      default: "https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png",
+    },
+  },
+};
+</script>
